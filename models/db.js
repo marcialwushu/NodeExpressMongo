a93f65325e3e2c15f6b23d2cddc2a9e7119f7680
cleilsonsousapereira <cleilsonpereira@unigrande.edu.br>
2019-10-09 11:52:09 -0300

    model for conect with mongoDB create

---
+++ models/db.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds331558.mlab.com:31558/employeedb', {userNewUrlParse: true}, (err) => {
+    if(!err) { console.log('MongoDB Conection Succeeded.') }
+    else { console.log('Error in DB conncetion :' + err) }
+});
