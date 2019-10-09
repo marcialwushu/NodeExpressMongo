381276978128a831a5987a8b6830951bb1aed9a7
cleilsonsousapereira <cleilsonpereira@unigrande.edu.br>
2019-10-09 14:22:35 -0300

    create views hbs template engine default for nodejs

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 require('./models/db');
 
 const express = require('express');
-
+const path = require('path');
 const employeeController = require('./controllers/employeeController');
 
 var app = express();
+
+app.set('views', path.join(__dirname, '/views/'));
+app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts' }));
+app.set('view engine', 'hbs');
 
 app.listen(3000, () => {
     console.log('Express server started at port : 3000');
